3906a2cdab2cbef349c9d81d2ddbfd894a289f69
banz-tenz <kesothin3@gmail.com>
2026-06-05 11:17:53 +0700

    feat:.env.example

--- src/config/database.ts
+++ src/config/database.ts
@@ -21,7 +21,7 @@ export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || "3306"),
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USERNAME || process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
